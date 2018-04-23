3519a839b89931f2f5fe292b73936fe87f1f8d0e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ExportInteractiveForms/Program.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Pdf;
+
+namespace ExportInteractiveForms {
+    class Program {
+        static void Main(string[] args) {
+
+            using (PdfDocumentProcessor processor = new PdfDocumentProcessor()) {
+
+                // Load a PDF document with AcroForm data. 
+                processor.LoadDocument("..\\..\\InteractiveForm.pdf");
+
+                // Export AcroForm data to XML format.
+                processor.Export("..\\..\\InteractiveForm.xml", PdfFormDataFormat.Xml);
+            }
+        }
+    }
+}
---
+++ VB/ExportInteractiveForms/Program.vb
@@ -0,0 +1,17 @@
+﻿Imports DevExpress.Pdf
+
+Namespace ExportInteractiveForms
+    Friend Class Program
+        Shared Sub Main(ByVal args() As String)
+
+            Using processor As New PdfDocumentProcessor()
+
+                ' Load a PDF document with AcroForm data. 
+                processor.LoadDocument("..\..\InteractiveForm.pdf")
+
+                ' Export AcroForm data to XML format.
+                processor.Export("..\..\InteractiveForm.xml", PdfFormDataFormat.Xml)
+            End Using
+        End Sub
+    End Class
+End Namespace
