f4261629cc0c3f1b7cc86abf8aac43c56542f92f
DevExpressExampleBot <fakeEmail>
2022-02-02 11:05:06 +0300

    Source auto update [skip ci]

--- VB/ExportInteractiveForms/Program.vb
+++ VB/ExportInteractiveForms/Program.vb
@@ -1,14 +1,13 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace ExportInteractiveForms
+
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
-            Using processor As New PdfDocumentProcessor()
-
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a PDF document with AcroForm data. 
                 processor.LoadDocument("..\..\InteractiveForm.pdf")
-
                 ' Export AcroForm data to XML format.
                 processor.Export("..\..\InteractiveForm.xml", PdfFormDataFormat.Xml)
             End Using
